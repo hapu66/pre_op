1a3b4a5d60317982c91888c7f55ff70334e135d6
hapu66 <hapu66@gmail.com>
2023-10-12 13:57:29 +0200

    starting with Table2

---
+++ t2.R
@@ -0,0 +1,76 @@
+
+source("preop.R")
+
+tGS2_act = d_act_nt6 %>%
+  filter(o_opmetode == 6) %>%
+  select(trt, a5_fu,    vent_a5,   ligg_a5,   reinn_a5,
+         alv_kmp_a5,  subst_a5,   depr_a5,   vtap_a5,   dBMI_a5) %>% 
+  tbl_summary(
+    by = trt,
+    statistic = list( a5_fu ~"{n}", 
+                      reinn_a5  ~ "{n} / {N} ({p}%)"  ,
+                      alv_kmp_a5~ "{n} / {N} ({p}%)"  ,
+                      subst_a5~ "{n} / {N} ({p}%)"  ,
+                      depr_a5~ "{n} / {N} ({p}%)" ),
+    label = list(a5_fu ~"Follow-up 5 yrs",
+                 vent_a5 ~"Waiting time ", 
+                 ligg_a5 ~"Postoperative days in hospital ", 
+                 reinn_a5 ~"Readmission ", 
+                 alv_kmp_a5 ~"Severe complication (30d) ", 
+                 subst_a5 ~"Substitution ", 
+                 depr_a5 ~"Depression ", 
+                 vtap_a5 ~"%TWL ", 
+                 dBMI_a5  ~"d BMI "),
+    missing_text = "Missing data" ) %>%  
+  add_p(test  = list(
+    gtsummary::all_continuous()  ~ "t.test", 
+    gtsummary::all_categorical() ~ "fisher.test") ) %>%
+  modify_header(update = all_stat_cols() ~ "**{level}**  \n N = {n}",
+                text_interpret ="md")
+
+
+tGB2_act = d_act_nt6 %>%
+  filter(o_opmetode == 1) %>%
+  select(trt, a5_fu,    vent_a5,   ligg_a5,   reinn_a5,
+         alv_kmp_a5,  subst_a5,   depr_a5,   vtap_a5,   dBMI_a5) %>% 
+  tbl_summary(
+    by = trt,
+    statistic = list( a5_fu ~"{n}", 
+                      reinn_a5  ~ "{n} / {N} ({p}%)"  ,
+                      alv_kmp_a5~ "{n} / {N} ({p}%)"  ,
+                      subst_a5~ "{n} / {N} ({p}%)"  ,
+                      depr_a5~ "{n} / {N} ({p}%)" ),
+    #   stat_fns = a5_fu ~my_cnt,
+    #    statistic = a5_fu ~ "{n}/ {a5_flw}  ",
+    #   type = list(a5_fu ~ 'continuous'),
+    label = list(a5_fu ~"Follow-up 5 yrs",
+                 vent_a5 ~"Waiting time ", 
+                 ligg_a5 ~"Postoperative days in hospital ", 
+                 reinn_a5 ~"Readmission ", 
+                 alv_kmp_a5 ~"Severe complication (30d) ", 
+                 subst_a5 ~"Substitution ", 
+                 depr_a5 ~"Depression ", 
+                 vtap_a5 ~"%TWL ", 
+                 dBMI_a5  ~"d BMI "),
+    missing_text = "Missing data" ) %>%  
+  add_p(test  = list(
+    gtsummary::all_continuous()  ~ "t.test", 
+    gtsummary::all_categorical() ~ "fisher.test") ) %>%
+  modify_header(update = all_stat_cols() ~ "**{level}**  \n N = {n}",
+                text_interpret ="md")
+
+
+tb_GS = d_elig %>% filter(o_opmetode == 6) %>% select(trt, o_preop_vektskole) %>%  tbl_summary(by=trt)  
+
+tb_GB = d_elig %>% filter(o_opmetode == 1) %>% select(trt, o_preop_vektskole) %>%  tbl_summary(by=trt)  
+
+###############################################  final table
+tA = tbl_stack(list(tb_GS, tGS2_act))
+tB = tbl_stack(list(tb_GB, tGB2_act))
+
+tbl_merge(list(tA,tB), tab_spanner = c("**Gastric Sleeve**", "**Gastric Bypass**" ) )  %>%
+  modify_header(update = all_stat_cols() ~ "**{level}**  \n N = {n}",
+                text_interpret ="md") %>% 
+  as_gt() %>%  gt::gtsave("tabell2.docx")
+
+

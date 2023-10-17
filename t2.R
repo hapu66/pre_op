0b1ae04ac87927bdb2876c66350d417d4694e77f
hapu66 <hapu66@gmail.com>
2023-10-17 14:09:16 +0200

    long term results

--- t2.R
+++ t2.R
@@ -36,8 +36,42 @@ shrt_res = function(tb) {tb %>%
   add_p() %>%
   add_ci()}
 
+
+lng_res = function(tb) {tb %>%
+  select(trt, a5_fu,    vent_a5,   ligg_a5,   reinn_a5,
+         alv_kmp_a5,  subst_a5,   depr_a5,   vtap_a5,   dBMI_a5) %>% 
+  tbl_summary(
+    by = trt,
+    statistic = list( a5_fu ~"{n}", 
+                      reinn_a5  ~ "{n} / {N} ({p}%)"  ,
+                      alv_kmp_a5~ "{n} / {N} ({p}%)"  ,
+                      subst_a5~ "{n} / {N} ({p}%)"  ,
+                      depr_a5~ "{n} / {N} ({p}%)" ),
+    label = list(a5_fu ~"Follow-up 5 yrs",
+                 vent_a5 ~"Waiting time ", 
+                 ligg_a5 ~"Postoperative days in hospital ", 
+                 reinn_a5 ~"Readmission ", 
+                 alv_kmp_a5 ~"Severe complication (30d) ", 
+                 subst_a5 ~"Substitution ", 
+                 depr_a5 ~"Depression ", 
+                 vtap_a5 ~"%TWL ", 
+                 dBMI_a5  ~"d BMI "),
+    missing_text = "Missing data" ) %>%  
+  add_p(test  = list(
+    gtsummary::all_continuous()  ~ "t.test", 
+    gtsummary::all_categorical() ~ "fisher.test") ) %>%
+  modify_header(update = all_stat_cols() ~ "**{level}**  \n N = {n}",
+                text_interpret ="md")}
+
+
+
 shrt_res(  d_act_GS_d30 )
 shrt_res(  d_act_GB_d30 )
+
+lng_res(  d_act_GS_nt6 )
+lng_res(  d_act_GB_nt6 )
+
+
   
 tGS2_act = d_act_GS_nt6 %>%
   select(trt, a5_fu,    vent_a5,   ligg_a5,   reinn_a5,

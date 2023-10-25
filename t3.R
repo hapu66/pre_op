8ccd3b77d371a3e082fbe2be793963aa75fc3104
hapu66 <hapu66@gmail.com>
2023-10-25 13:17:22 +0200

    fu

--- t3.R
+++ t3.R
@@ -78,7 +78,7 @@ l_GS |> as_gt() |>
               "p.value" = NA ,   
               .before = 2 )  
  # --------------------------  Follow-up %:s   format
-l_GS |> as_gt() |> 
+l_GS  = l_GS |> as_gt() |> 
   rows_add( .list = rlang::list2(  "label" = "potential 5 yr",
               "stat_1" = as.character( N_op_a5[1,2]),
               "stat_2" = as.character( N_op_a5[2,2]),
@@ -94,7 +94,7 @@ M_GS = matrix(c(685, 676, 1222, 1066),
 fisher.test(M_GS)
 #--
 
-l_GB |> as_gt() |> 
+l_GB = l_GB |> as_gt() |> 
   rows_add( .list = rlang::list2(  "label" = "potential 5 yr",
                                    "stat_1" = as.character( N_op_a5[3,2]),
                                    "stat_2" = as.character( N_op_a5[4,2]),
@@ -117,4 +117,33 @@ fisher.test(M_GB)
 # tbl_stack( list(sht_res(d_act_GS_d30), lng_res(d_act_GS_nt6)))
   #   merge tables
 
-tibble("label"= "rw", "stat_1" = N_op_a5[1,2], "stat_2" =  N_op_a5[2,2], "p.value" =0.05)
+#  tibble("label"= "rw", "stat_1" = N_op_a5[1,2], "stat_2" =  N_op_a5[2,2], "p.value" =0.05)
+tab3_GS =  tbl_stack(list(s_GS, l_GS))
+
+M_GS_s = matrix(c(2178, 3217, 2353, 3392),
+              nrow = 2)
+fisher.test(M_GS_s)
+
+
+
+tab3_GS  |> as_gt() |> 
+  rows_add( .list = rlang::list2(  "label" = "potential 30 d",
+                                   "stat_1" = as.character( N_op_d30[1,2]),
+                                   "stat_2" = as.character( N_op_d30[2,2]),
+                                   "p.value" = NA ),   
+            .before = 1 )  |>
+  rows_add( .list = rlang::list2(  "label" = "follow-up %; 30 d",
+                                   "stat_1" = as.character( s_GS$df_by$n[1]/ N_op_d30[1,2]),
+                                   "stat_2" = as.character( s_GS$df_by$n[2]/ N_op_d30[2,2]),
+                                   "p.value" = 0.5369 ),   
+            .before = 2 ) |>
+  rows_add( .list = rlang::list2(  "label" = "potential 5 yr",
+                                   "stat_1" = as.character( N_op_a5[1,2]),
+                                   "stat_2" = as.character( N_op_a5[2,2]),
+                                   "p.value" = NA ),   
+            .before = 13 )  |>
+  rows_add( .list = rlang::list2(  "label" = "follow-up %; 5 yrs",
+                                   "stat_1" = as.character( l_GS$df_by$n[1]/ N_op_a5[1,2]),
+                                   "stat_2" = as.character( l_GS$df_by$n[2]/ N_op_a5[2,2]),
+                                   "p.value" = 0.07479  ),   
+            .before = 14 )  

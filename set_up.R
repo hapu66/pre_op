83fa974acb98b5f496d4f456bd081054da8c6e7f
hapu66 <hapu66@gmail.com>
2023-10-31 09:48:48 +0100

    as.Date("2023-10-31") - months(6) [1] NA

--- set_up.R
+++ set_up.R
@@ -192,31 +192,26 @@ dt =  d_prim %>%
            o_preop_vektskole==1 & o_opmetode ==6 ~ "GS school",
            o_preop_vektskole==0 & o_opmetode ==6 ~ "GS norm"))
 
-# dt %<>% mutate( vent_a5 = ifelse(a5_fu, vent, NA_integer_),
-#                 ligg_a5 = ifelse(a5_fu, ligg, NA_integer_),
-#                 alv_kmp_a5 = ifelse(a5_fu, alv_kmp, NA),
-#                 subst_a5 = ifelse(a5_fu, subst, NA),
-#                 reinn_a5 = ifelse(a5_fu, reinn, NA),
-#                 depr_a5 = ifelse(a5_fu, depr, NA),
-#                 vtap_a5 = ifelse(a5_fu, vtap, NA_real_),
-#                 dBMI_a5 = ifelse(a5_fu, dBMI, NA_real_))
-########################################    select variables  
-
 d =    dt %>% filter(!is.na(o_preop_vektskole), o_opmetode %in% c(1, 6)) %>%
   select(p_pasientid, ForlopsID, o_sykehus,  o_dato_op, p_alder_v_op, Sex, Female, 
          o_preop_vektskole, o_preop_vektprog, o_opmetode, smoke, work,
          bi_finans,  u6_ferdigstill, u6_oppf_type, a5_oppf_type,
          contains("bmi_"),  contains("b_beh"),  vt_pr, TWL_pr,
          vent,   ligg,   alv_kmp,  subst,  N_revop,
-         reinn,  depr,   vtap,   dBMI,   u6_fu, a5_fu, 
-         o_dato_op, trt, a5_ferdigstill, a5_ant_vekt, a5_dato_oppf, bmi_5a,  a5_TWL) %>% 
+         reinn,  depr,   vtap,   dBMI,   u6_fu, a5_fu, trt,
+         o_dato_op, a5_ferdigstill, a5_ant_vekt, a5_dato_oppf, bmi_5a,  a5_TWL) %>% 
   mutate(bmi_0 = bmi_baseline, bmi_0o = bmi_op, bmi_0u6 = bmi_6v) %>%
   select(-bmi_baseline, -bmi_op, -bmi_6v)
 
 eo_dato_d30 = as.Date("2023-06-30")
 eo_dato_a5 = as.Date("2018-04-15")
 
-d_elig    = d %>% filter(o_dato_op <  Sys.Date() - years(5) - months(6))      #  
+#  Sys.Date() - years(5) - months(6)
+#  [1] NA         on 2023-10-31
+#
+## d_elig    =  d %>% filter(o_dato_op <  Sys.Date() - years(5) - months(6))  
+#
+d_elig    =  d %>% filter(o_dato_op <  Sys.Date() - days(2007))  # 5.5 yr
 d_elig_GS  = d_elig  %>% filter(o_opmetode == 6)
 d_elig_GB  = d_elig  %>% filter(o_opmetode == 1)
 
@@ -227,7 +222,7 @@ d_elig_GB_d30 = d_elig_d30 %>% filter(o_opmetode == 1)
 
 
 
-d_act_a5 = d %>% filter(o_dato_op <  Sys.Date() - years(5) - months(6), 
+d_act_a5 = d %>% filter(o_dato_op <  Sys.Date() - days(2007), 
                      a5_ferdigstill == 1,                                                      # a5_oppf_type %in% c(1, 2, 3),  # !!  BESTILL   a5_NUM
                      a5_oppf_type %in%  c("Frammøte", "Per telefon eller via nettmøte",  "Per brev/mail eller på annen måte"),   ###   kod = 1,2,3
                      !is.na(bmi_5a),

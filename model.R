d489437eedf96c9b79ddb135caafea50822e3015
hapu66 <hapu66@gmail.com>
2023-11-23 10:38:50 +0100

    plot alle sh + LME model

--- model.R
+++ model.R
@@ -2,7 +2,7 @@
 library(lme4)
 library(lmerTest)
 
-simp =  lmer(a5_TWL ~  p_alder_v_op+ o_preop_vektskole + (1|o_sykehus), data = d_act_a5)
+simp =  lmer(a5_TWL ~  p_alder_v_op + o_preop_vektskole + (1|o_sykehus), data = d_act_a5)
 d_act_a5$fixed <- predict(simp, re.form=NA)
 d_act_a5$rand <- predict(simp)
  
@@ -12,8 +12,76 @@ gr1 = gr0 + geom_point()
 gr1
 gr1<-gr1+geom_line(aes(x=p_alder_v_op,y=fixed),colour=2, lwd=1) 
 gr1<-gr1+geom_line(aes(x=p_alder_v_op,y=rand),colour=4, lwd=1) 
-gr1<-gr1+labs(y = ylabel,x=xlabel,title="Fixed and random effects")
+gr1<-gr1+labs(y = "%TWL", x="Opr.age", title="Fixed and random effects")
 gr1+facet_wrap("o_sykehus") 
+
+# -----------
+
+dee = d_act_a5_GS |> filter(!is.na(vt_pr))  # 8 st vt_pr  missing
+
+cplx =  lmer(a5_TWL ~ vt_pr + p_alder_v_op + Female + bmi_0 + o_preop_vektskole + (1|o_sykehus), data = dee)
+dee$fixed <- predict(cplx, re.form=NA)
+dee$rand <- predict(cplx)
+
+gr0 = ggplot(data = dee, aes(x=p_alder_v_op, y=a5_TWL))
+gr1 = gr0 + geom_point()
+
+gr1
+gr1<-gr1+geom_line(aes(x=p_alder_v_op, y=fixed),colour=2, lwd=1) 
+gr1<-gr1+geom_line(aes(x=p_alder_v_op, y=rand),colour=4, lwd=1) 
+gr1<-gr1+labs(y = "%TWL", x="Opr.age", title="Fixed and random effects")
+gr1+facet_wrap("o_sykehus") 
+# ----------------------------------------------
+
+pp_sh = function(df){
+  dee = df  |> filter( !is.na(vt_pr), !is.na(o_preop_vektprog))
+  ch_nm = deparse(substitute(df))
+  sts  = ifelse(str_detect(ch_nm, "GS"),"Sleeve", 
+                ifelse(str_detect(ch_nm, "GB"),"Bypass", "not GS nor GB!!"))
+  mg = paste0(sts,  ", 5 yr WL(%) vs. opr.age")
+  
+  model1 <- lmer(formula = a5_TWL ~ vt_pr + p_alder_v_op + Female + bmi_0 + o_preop_vektprog + o_preop_vektskole + (1|o_sykehus), 
+                 data    = dee)
+M=   summary(model1)  
+  
+
+  dee$fixed <- predict(model1, re.form=NA)
+  dee$rand <- predict(model1)
+print(M) 
+  
+G = ggplot(data = dee,
+       aes(x = p_alder_v_op,
+           y = a5_TWL,
+           col = as.factor( o_sykehus)))+
+  viridis::scale_color_viridis(discrete = TRUE)+
+  geom_point(size = 1.6, alpha = 0.8) +
+##  geom_smooth(method = lm,              col = "black",              size = .5,              alpha = 0.8)+
+  theme_minimal()
+#  scale_color_gradientn(colours = rainbow(100))+
+G<-G+geom_smooth(method = lm, aes(x=p_alder_v_op, y=fixed),colour=4, lwd=1) 
+G<-G+geom_smooth(method = lm, aes(x=p_alder_v_op, y=rand),colour= 2, lwd=1) 
+
+  G = G+labs(title = mg,
+       subtitle = "blue line: fixed effects, red line: random effect",
+       xlab = "Age at operation", ylab = " 5 years %TWL", col ="Hospital" ) #+    scale_fill_discrete(name = )
+  
+G + facet_wrap("o_sykehus") 
+}
+
+pp_sh(d_act_a5_GS)
+pp_sh(d_act_a5_GB)
+
+model1 <- lmer(formula = a5_TWL ~ vt_pr + p_alder_v_op + Female + bmi_0 + o_preop_vektskole + (1|o_sykehus), 
+               data    = d_act_a5_GS)
+summary(model1)
+
+dee = d_act_a5_GS  |> filter( !is.na(vt_pr))
+dee$fixed <- predict(model1, re.form=NA)
+dee$rand <- predict(model1)
+
+
+
+
 
 
 m_WL5 =  lmer(a5_TWL ~  p_alder_v_op + Female + o_preop_vektskole + (1|o_sykehus), data = d_act_a5)

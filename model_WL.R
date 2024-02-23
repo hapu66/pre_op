997b3c46b748ce810da9341dd408b3a4d2f33bb5
hapu66 <hapu66@gmail.com>
2024-02-23 14:29:11 +0100

    comments

--- model_WL.R
+++ model_WL.R
@@ -119,7 +119,7 @@ bmi_a5( d_a5_bmi_j |> filter(o_opmetode == 1))
 
 
 m_0   <- lmer(formula = a5_TWL ~ p_alder_v_op + Female + bmi_0 + o_opmetode + o_preop_vektskole + b_beh_diab + smoke +(1|o_sykehus),  
-              data = d_elig |> filter(a5_nt))  # d_act_a5 has NOT -5.5år -filter
+              data = d_elig |> filter(a5_nt))  #  
 summary(m_0)
 REff  = ranef(m_0)
 RE_tbbl = as_tibble(REff)
@@ -166,12 +166,12 @@ m_vkt_0_GS <-  lm(formula = a5_ant_vekt ~ b_ant_vekt +  p_alder_v_op + Female  +
     data = d_elig_GS |> filter(a5_nt))  #   
  summary(m_vkt_0_GS) 
  
- m_vkt_0_GB <-  lm(formula = a5_ant_vekt ~ b_ant_vekt +  p_alder_v_op + Female  + o_preop_vektskole + b_beh_diab + smoke  ,  # remove bmi_0
-                   data = d_elig_GB |> filter(a5_nt))  #   
+m_vkt_0_GB <-  lm(formula = a5_ant_vekt ~ b_ant_vekt +  p_alder_v_op + Female  + o_preop_vektskole + b_beh_diab + smoke  ,  # remove bmi_0
+   data = d_elig_GB |> filter(a5_nt))  #   
  summary(m_vkt_0_GB) 
 # --- 
  
-  
+# -----------------------------------  random effects  
 m_vkt_GS   <- lmer(formula = a5_ant_vekt ~ b_ant_vekt +  p_alder_v_op + Female  + o_preop_vektskole + b_beh_diab + smoke +(1|o_sykehus),  # remove bmi_0
    data = d_elig_GS |> filter(a5_nt))  #  
   summary(m_vkt_GS)
@@ -184,6 +184,9 @@ m_vkt_GB   <- lmer(formula = a5_ant_vekt ~ b_ant_vekt +  p_alder_v_op + Female  
   REff_vkt_GB  = ranef(m_vkt_GB)
   RE_tbbl_vkt_GB = as_tibble(REff_vkt_GB)
   
+dotplot(ranef(m_vkt_GS))  
+dotplot(ranef(m_vkt_GB))  
+
   #        ----------------------------- weight adjusting RE hospitals at a5 
   vkt_just_GS = function(o_sykehus){
     RE_tbbl_vkt_GS |> filter(grp == o_sykehus) |> pull(condval)
@@ -220,25 +223,26 @@ rs_a5(d_a5_vkt_GB_j)
   (mean(da_GB$b_ant_vekt) - mean(da_GB$a5_ant_vekt))/mean(da_GB$b_ant_vekt)*100
   
 
-  # -------------------------------------------------------  bmi  2024-02-23
+  # -------------------------------------------------------  bmi  2024-02-23 ----------------------------------------------------------------------
   # 
-  m_bmi_0_GS <-  lm(formula = bmi_5a ~ bmi_0 +  p_alder_v_op + Female  + o_preop_vektskole + b_beh_diab + smoke  ,  # remove bmi_0
+  m_bmi_0_GS <-  lm(formula = bmi_5a ~ bmi_0 +  p_alder_v_op + Female  + o_preop_vektskole + b_beh_diab + smoke  ,  #  
                     data = d_elig_GS |> filter(a5_nt))  #   
   summary(m_bmi_0_GS) 
   
-  m_bmi_0_GB <-  lm(formula = bmi_5a ~ bmi_0 +  p_alder_v_op + Female  + o_preop_vektskole + b_beh_diab + smoke  ,  # remove bmi_0
+  m_bmi_0_GB <-  lm(formula = bmi_5a ~ bmi_0 +  p_alder_v_op + Female  + o_preop_vektskole + b_beh_diab + smoke  ,  #  
                     data = d_elig_GB |> filter(a5_nt))  #   
   summary(m_bmi_0_GB) 
   # --- 
   
+  # -----------------------------------  random effects  
   
-  m_bmi_GS   <- lmer(formula = bmi_5a ~ bmi_0 +  p_alder_v_op + Female  + o_preop_vektskole + b_beh_diab + smoke +(1|o_sykehus),  # remove bmi_0
+  m_bmi_GS   <- lmer(formula = bmi_5a ~ bmi_0 +  p_alder_v_op + Female  + o_preop_vektskole + b_beh_diab + smoke +(1|o_sykehus),  #  
                      data = d_elig_GS |> filter(a5_nt))  #  
   summary(m_bmi_GS)
   REff_bmi_GS  = ranef(m_bmi_GS)
   RE_tbbl_bmi_GS = as_tibble(REff_bmi_GS)
   
-  m_bmi_GB   <- lmer(formula = bmi_5a ~ bmi_0 +  p_alder_v_op + Female  + o_preop_vektskole + b_beh_diab + smoke +(1|o_sykehus),  # remove bmi_0
+  m_bmi_GB   <- lmer(formula = bmi_5a ~ bmi_0 +  p_alder_v_op + Female  + o_preop_vektskole + b_beh_diab + smoke +(1|o_sykehus),  #  
                      data = d_elig_GB |> filter(a5_nt))  #  
   summary(m_bmi_GB)
   REff_bmi_GB  = ranef(m_bmi_GB)
@@ -268,26 +272,27 @@ rs_a5(d_a5_vkt_GB_j)
   
   # -------------------------------------------------------  TWL  2024-02-23
   # 
-  m_TWL_0_GS <-  lm(formula = a5_TWL ~ bmi_0 +  p_alder_v_op + Female  + o_preop_vektskole + b_beh_diab + smoke  ,  # remove bmi_0
-                    data = d_elig_GS |> filter(a5_nt))  #   
+  m_TWL_0_GS <-  lm(formula = a5_TWL ~ bmi_0 +  p_alder_v_op + Female  + o_preop_vektskole + b_beh_diab + smoke  ,  #  
+    data = d_elig_GS |> filter(a5_nt))  #   
   summary(m_TWL_0_GS) 
   
-  m_TWL_0_GB <-  lm(formula = a5_TWL ~ bmi_0 +  p_alder_v_op + Female  + o_preop_vektskole + b_beh_diab + smoke  ,  # remove bmi_0
-                    data = d_elig_GB |> filter(a5_nt))  #   
+  m_TWL_0_GB <-  lm(formula = a5_TWL ~ bmi_0 +  p_alder_v_op + Female  + o_preop_vektskole + b_beh_diab + smoke  ,  #  
+    data = d_elig_GB |> filter(a5_nt))  #   
   summary(m_TWL_0_GB) 
   # --- 
   
+  # -----------------------------------  random effects  
   
-  m_TWL_GS   <- lmer(formula = a5_TWL ~ bmi_0 +  p_alder_v_op + Female  + o_preop_vektskole + b_beh_diab + smoke +(1|o_sykehus),  # remove bmi_0
-                     data = d_elig_GS |> filter(a5_nt))  #  
+  m_TWL_GS   <- lmer(formula = a5_TWL ~ bmi_0 +  p_alder_v_op + Female  + o_preop_vektskole + b_beh_diab + smoke +(1|o_sykehus),  #  
+   data = d_elig_GS |> filter(a5_nt))  #  
   summary(m_TWL_GS)
   REff_TWL_GS  = ranef(m_TWL_GS)
   RE_tbbl_TWL_GS = as_tibble(REff_TWL_GS)
   
   dotplot( ranef(m_TWL_GS))
   
-  m_TWL_GB   <- lmer(formula = a5_TWL ~ bmi_0 +  p_alder_v_op + Female  + o_preop_vektskole + b_beh_diab + smoke +(1|o_sykehus),  # remove bmi_0
-                     data = d_elig_GB |> filter(a5_nt))  #  
+  m_TWL_GB   <- lmer(formula = a5_TWL ~ bmi_0 +  p_alder_v_op + Female  + o_preop_vektskole + b_beh_diab + smoke +(1|o_sykehus),  #  
+    data = d_elig_GB |> filter(a5_nt))  #  
   summary(m_TWL_GB)
   REff_TWL_GB  = ranef(m_TWL_GB)
   RE_tbbl_TWL_GB = as_tibble(REff_TWL_GB)
@@ -305,14 +310,14 @@ rs_a5(d_a5_vkt_GB_j)
   
   d_a5_TWL_GS_j = d_elig_GS |> filter(a5_nt) |> 
     mutate(a5_TWL_j =  a5_TWL - o_sykehus |> map(TWL_just_GS) |> unlist(),
-           vtap = a5_TWL_j)
+   vtap = a5_TWL_j)
   
   rs_a5(d_a5_TWL_GS_j)
   
   #-- GB
   d_a5_TWL_GB_j = d_elig_GB |> filter(a5_nt) |> 
     mutate(a5_TWL_j =  a5_TWL - o_sykehus |> map(TWL_just_GB) |> unlist(),
-           vtap = a5_TWL_j)
+   vtap = a5_TWL_j)
   
   rs_a5(d_a5_TWL_GB_j)
   
